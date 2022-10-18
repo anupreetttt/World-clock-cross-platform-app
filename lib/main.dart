31a4ca8f1b4744f5114512dcb3056718a5ed944b
Anupreet Paulkar <apaulk2@uic.edu>
2022-10-18 02:55:36 -0500

    Floaing button

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,39 @@
 import 'package:flutter/material.dart';
 
-void main() {
+void main() =>
   runApp(MaterialApp(
-    home: Scaffold(
+    home: homeScreen()
+  ));
+
+class homeScreen extends StatelessWidget {
+  // const ({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       appBar: AppBar(
         title: Text('Hello world'),
         centerTitle: true,
+        backgroundColor: Colors.teal[600],
       ),
-    ),
-  ));
+      body: const Center(
+        child: Text(
+          'Hello',
+          style: TextStyle(
+            fontSize: 30.0,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2.0,
+            color: Colors.red,
+
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Text('Clicked me'),
+        backgroundColor: Colors.teal[600],
+      ),
+    );
+  }
 }
+
+
